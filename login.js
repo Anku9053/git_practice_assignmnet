99682d7c875a94e1e0916ef274c3819c01348ebb
Dharmik Puri Goswami <dharmikpuri2004@gmail.com>
2022-09-13 23:37:35 +0530

    correction logic

--- login.js
+++ login.js
@@ -1,25 +1,25 @@
 //prime number
 let N=1635;
 let count=0;
-for(i=1;i<N;i++){
-if(i%i=0){
+for(i=0;i<N;i++){
+if(N%i=0){
 count++
 }
 }if(count==2){
-console.log("Not Prime")
+console.log("Prime");
 }else{
-console.log("Prime")
+console.log("Not Prime");
 }
 
 //palindrome
 
 let str=["level"];
  let bag = "";
-   for(i=str.length-1;0>=i;i++){
+   for(i=str.length-1;0>=i;i--){
        bag = bag + str[i];
-   }if(bag = str){
-       console.log("No");
-   }else{
+   }if(bag == str){
        console.log("Yes");
+   }else{
+       console.log("No");
    }
 
