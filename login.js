18dfccfa2ef6ee384b82d7a251c02b9abadcf96b
Ankesh Kumar <ankesh9053@gmail.com>
2022-09-13 22:48:39 +0530

    Added prime numbers and pallindrone question

---
+++ login.js
@@ -0,0 +1,25 @@
+//prime number
+let N=1635;
+let count=0;
+for(i=1;i<N;i++){
+if(i%i=0){
+count++
+}
+}if(count==2){
+console.log("Not Prime")
+}else{
+console.log("Prime")
+}
+
+//palindrome
+
+let str=["level"];
+ let bag = "";
+   for(i=str.length-1;0>=i;i++){
+       bag = bag + str[i];
+   }if(bag = str){
+       console.log("No");
+   }else{
+       console.log("Yes");
+   }
+
